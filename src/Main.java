547211839b086575bc24ebba5c034afc2c5816da
Ig0rSavenko92 <igor.savenko92@mail.ru>
2023-11-07 00:37:57 +0300

    обновление 1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        myScaner ();
-    }
-    public static void myScaner () {
         int count = 1;
-        System.out.println("Путь к файлу:");
         while (true) {
+            System.out.println("Путь к файлу:");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExist = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (fileExist==false)
-            System.out.println("Указанный файл не существуе");
-            if (isDirectory==true)
-            System.out.println("Указанный путь является путём к папке, а не к файлу");
-            if (fileExist==true && isDirectory==false) System.out.println("Путь указан верно. " +"Это файл номер "+ count++);
-            if (fileExist==true && isDirectory==false) continue;
-            System.out.println("Путь к файлу:");
+            if (fileExist == false)
+                System.out.println("Указанный файл не существуе");
+            if (isDirectory == true)
+                System.out.println("Указанный путь является путём к папке, а не к файлу");
+            if (fileExist == true && isDirectory == false)
+                System.out.println("Путь указан верно. " + "Это файл номер " + count++);
+            if (fileExist == true && isDirectory == false) continue;
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                int lines = 0;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    lines++;
+                }
+                System.out.println(lines);
+            } catch (FileNotFoundException ex) {
+                throw new IllegalArgumentException("Ошибка ресурса", ex);
+            }
+            catch (IOException ex) {
+            throw new IllegalArgumentException("Ресурс отсутствует", ex);
         }
     }
+}
 }
